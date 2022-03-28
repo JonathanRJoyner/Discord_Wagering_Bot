f272a510ed747aa2ebc61eb20e070f089ebe9590
Jonathan Joyner <jonathan.joyner94@gmail.com>
2022-03-28 08:57:11 -0500

    Edited to improve readability. Added new _bet_str(

--- interaction.py
+++ interaction.py
@@ -8,9 +8,123 @@ from dotenv import load_dotenv, find_dotenv
 import db
 
 load_dotenv(find_dotenv())
+
 TOKEN = os.getenv('TOKEN')
 bot = discord.Bot()
 
+async def get_sports(ctx: discord.AutocompleteContext):
+    """Returns a list of sports that begin with the characters entered so far."""
+    return [sport for sport in db.sports() if sport.startswith(ctx.value)]
+
+async def get_leagues(ctx: discord.AutocompleteContext):
+    """Returns a list of sports that begin with the characters entered so far."""
+    return [
+        league
+        for league in db.leagues(ctx.options["sport"])
+        if league.startswith(ctx.value)
+    ]
+
+async def get_match(ctx: discord.AutocompleteContext):
+    """Returns a list of teams that are within the selected sport option"""
+    return [
+        f"{match[3]} vs. {match[4]} | {match[2]}"
+        for match in db.matches(ctx.options["league"])
+    ]
+
+async def get_bets(ctx: discord.AutocompleteContext):
+    return [bet for bet in db.bets(ctx.options["match"])]
+
+@bot.slash_command(guild_ids=[888919487255031848, 272141605421580288])
+async def wager(
+    ctx: discord.ApplicationContext,
+    sport: Option(
+        str, 
+        "Pick a league!", 
+        autocomplete=discord.utils.basic_autocomplete(get_sports)
+    ),
+    league: Option(
+        str,
+        "Pick a league!",
+        autocomplete=discord.utils.basic_autocomplete(get_leagues),
+    ),
+    match: Option(
+        str, 
+        "Pick a match!", 
+        autocomplete=discord.utils.basic_autocomplete(get_match)
+    ),
+    bet: Option(
+        str, 
+        "Pick a bet!", 
+        autocomplete=discord.utils.basic_autocomplete(get_bets)
+    ),
+    bet_amount: Option(
+        int,
+        "Pick an amount!",
+        min_value=0,
+    ),  # max_amount=user['amount'] eventually)
+):
+    """This is the main wager command. Users input a sport, league, match, bet, and bet amount.
+    The confirmation view is called with details about their wager."""
+    view = Confirm()
+
+    user = [str(ctx.author.id), str(ctx.author.name)]
+    user_amount = db.user_lookup(user)[0][-1]
+
+    if bet_amount > user_amount:
+        await ctx.respond(f"You only have {user_amount} points", ephemeral=True)
+
+    payout = _payout(bet, bet_amount)
+    gametime = _gametime(match)
+    match = _match(match)
+    teams = _teams(match)
+    team_choice = _team_choice(bet)
+
+    # response embed
+    info_embed = discord.Embed(
+        title=match, color=0x00FF00
+    )
+    info_embed.set_author(
+        name=ctx.author.display_name, icon_url=ctx.author.display_avatar
+    )
+    info_embed.add_field(
+        name="Game Time", value=gametime, inline=False
+    )
+    info_embed.add_field(
+        name="Your Team", value=team_choice, inline=False
+    )
+    info_embed.add_field(
+        name="Your Bet", value=bet_amount, inline=True
+    )
+    info_embed.add_field(
+        name="Payout", value=payout, inline=True
+    )
+    info_embed.add_field(
+        name="_", value='View this project on [GitHub](https://github.com/JonathanRJoyner/Discord_Wagering_Bot).', inline=False
+    )
+
+    # responding to the confirmation
+    await ctx.send_response("Confirm your wager:", view=view, embed=info_embed)
+
+    await view.wait()
+
+    if view.value is None:
+        pass
+    elif view.value:
+        data = (
+            user[0],
+            league,
+            gametime,
+            teams[0],
+            teams[1],
+            team_choice,
+            bet_amount,
+            payout,
+            None,
+            None,
+        )
+        db.user_bet(data)
+    else:
+        pass
 
 class Confirm(discord.ui.View):
     """This is a class which creates a confimation menu. If the 'confirm' button is pressed, the inner value is set to true.
@@ -31,110 +145,49 @@ class Confirm(discord.ui.View):
         self.stop()
 
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.grey)
-    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+    async def cancel(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
         await interaction.response.send_message(
             "The wager has been cancelled.", ephemeral=True
         )
         self.value = False
         self.stop()
 
-
-async def get_sports(ctx: discord.AutocompleteContext):
-    """Returns a list of sports that begin with the characters entered so far."""
-    return [sport for sport in db.sports() if sport.startswith(ctx.value)]
-
-
-async def get_leagues(ctx: discord.AutocompleteContext):
-    """Returns a list of sports that begin with the characters entered so far."""
-    return [
-        league
-        for league in db.leagues(ctx.options["sport"])
-        if league.startswith(ctx.value)
-    ]
-
-
-async def get_match(ctx: discord.AutocompleteContext):
-    """Returns a list of teams that are within the selected sport option"""
-    return [
-        f"{match[3]} vs. {match[4]} | {match[2]}"
-        for match in db.matches(ctx.options["league"])
-    ]
-
-
-async def get_bets(ctx: discord.AutocompleteContext):
-    return [bet for bet in db.bets(ctx.options["match"])]
-
-
 @bot.slash_command(guild_ids=[888919487255031848, 272141605421580288])
-async def wager(
-    ctx: discord.ApplicationContext,
-    sport: Option(
-        str, "Pick a league!", autocomplete=discord.utils.basic_autocomplete(get_sports)
-    ),
-    league: Option(
-        str,
-        "Pick a league!",
-        autocomplete=discord.utils.basic_autocomplete(get_leagues),
-    ),
-    match: Option(
-        str, "Pick a match!", autocomplete=discord.utils.basic_autocomplete(get_match)
-    ),
-    bet: Option(
-        str, "Pick a bet!", autocomplete=discord.utils.basic_autocomplete(get_bets)
-    ),
-    bet_amount: Option(
-        int,
-        "Pick an amount!",
-        min_value=0,
-    ),  # max_amount=user['amount'] eventually)
-):
-    """This is the main wager command. Users input a sport, league, match, bet, and bet amount.
-    The confirmation view is called with details about their wager."""
-    view = Confirm()
+async def user_status(ctx):
+    '''Returns the users amount along with any unpaid wagers.'''
     user = [str(ctx.author.id), str(ctx.author.name)]
     user_amount = db.user_lookup(user)[0][-1]
-    if bet_amount > user_amount:
-        await ctx.respond(f"You only have {user_amount} points", ephemeral=True)
+    bets = db.unpaid_bets_lookup(str(ctx.author.id))
+    bet_str = _bet_str(bets)
 
-    payout = _payout(bet, bet_amount)
-    gametime = _gametime(match)
-    match = _match(match)
-    teams = _teams(match)
-    team_choice = _team_choice(bet)
-
-    # embed info
-    info_embed = discord.Embed(title=match, color=0x00FF00)
+    #embed info
+    info_embed = discord.Embed(title='User Status', color=0x00FF00)
     info_embed.set_author(
         name=ctx.author.display_name, icon_url=ctx.author.display_avatar
     )
-    info_embed.add_field(name="Game Time", value=gametime, inline=False)
-    info_embed.add_field(name="Your Team", value=team_choice, inline=False)
-    info_embed.add_field(name="Your Bet", value=bet_amount, inline=True)
-    info_embed.add_field(name="Payout", value=payout, inline=True)
-    info_embed.add_field(name="_", value='View this project on [GitHub](https://github.com/JonathanRJoyner/Discord_Wagering_Bot).', inline=False)
+    info_embed.add_field(
+        name="Your Points", value=user_amount, inline=False
+    )
+    info_embed.add_field(
+        name="Gametime | Team Choice | Bet Amount, Reward", value=bet_str, inline=False
+    )    
+    info_embed.add_field(
+        name="_", value='View this project on [GitHub](https://github.com/JonathanRJoyner/Discord_Wagering_Bot).', inline=False
+    )
+    await ctx.send_response(
+        ' ',embed=info_embed
+    )
 
-    # responding to the command
-    await ctx.send_response("Confirm your wager:", view=view, embed=info_embed)
-    await view.wait()
-    if view.value is None:
-        pass
-    elif view.value:
-        data = (
-            user[0],
-            league,
-            gametime,
-            teams[0],
-            teams[1],
-            team_choice,
-            bet_amount,
-            payout,
-            None,
-            None,
-        )
-        db.user_bet(data)
-    else:
-        pass
-
+def _bet_str(bets):
+    '''Takes user bets and outputs as a string.'''
+    bet_str = ''
+    for bet in bets:
+        bet = f'{bet[2]} | {bet[5]} | {bet[6]}, {bet[7]}\n'
+        bet_str += bet
+    
+    return bet_str
 
 def _payout(bet, amount):
     '''Calculates the payout given the betting odds and the amount a user wagers.'''
@@ -167,27 +220,5 @@ def _teams(match):
 def _team_choice(bet):
     '''Gets the odds for the users selected bet.'''
     return bet.split(" | ")[0]
-
-@bot.slash_command(guild_ids=[888919487255031848, 272141605421580288])
-async def user_status(ctx):
-    '''Returns the users amount along with any unpaid wagers.'''
-    user = [str(ctx.author.id), str(ctx.author.name)]
-    user_amount = db.user_lookup(user)[0][-1]
-    bets = db.unpaid_bets_lookup(str(ctx.author.id))
-
-    bet_str = ''
-    for bet in bets:
-        bet = f'{bet[2]} | {bet[5]} | {bet[6]}, {bet[7]}\n'
-        bet_str += bet
-
-    #embed info
-    info_embed = discord.Embed(title='User Status', color=0x00FF00)
-    info_embed.set_author(
-        name=ctx.author.display_name, icon_url=ctx.author.display_avatar
-    )
-    info_embed.add_field(name="Your Points", value=user_amount, inline=False)
-    info_embed.add_field(name="Gametime | Team Choice | Bet Amount, Reward", value=bet_str, inline=False)    
-    info_embed.add_field(name="_", value='View this project on [GitHub](https://github.com/JonathanRJoyner/Discord_Wagering_Bot).', inline=False)
-    await ctx.send_response(' ',embed=info_embed)
 
 bot.run(TOKEN)
