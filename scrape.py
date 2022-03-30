2e2ab9850c62ab73a9e277cb53846b7fbf773eef
Jonathan Joyner <jonathan.joyner94@gmail.com>
2022-03-30 11:48:41 -0500

    Changed selenium to headless mode

--- scrape.py
+++ scrape.py
@@ -13,7 +13,7 @@ chromedriver_autoinstaller.install()
 
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
-#options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 
 class UpcomingMatch:
